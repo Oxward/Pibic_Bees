88fc3045d073a189069b4e9f97a838ab6ae00a54
AndersonFdS <andersonfreiretads@gmail.com>
2018-09-10 17:00:53 -0300

    Initializing Map Markers with Database

--- app/src/main/java/andersonfds/pibic/Activities/RegisterOcActivity.java
+++ app/src/main/java/andersonfds/pibic/Activities/RegisterOcActivity.java
@@ -1,27 +1,38 @@
 package andersonfds.pibic.Activities;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.nio.file.Files;
 
 import andersonfds.pibic.R;
 
 public class RegisterOcActivity extends AppCompatActivity
 {
 
+    private ImageView imageView;
+    public static final int REQUEST_IMAGE_CAPTURE = 101;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_oc);
+        imageView = findViewById( R.id.imgTiraFoto );
     }
 
-    public void buttonPress(View view)
+    public void buttonPress( View view )
     {
         switch ( view.getId() )
         {
             case R.id.imgTiraFoto:
+                takePicture(view);
                 break;
 
             case R.id.btGal:
@@ -36,7 +47,7 @@ public class RegisterOcActivity extends AppCompatActivity
         }
     }
 
-    public void buttonTeste( View v)
+    public void buttonTeste( View v )
     {
         if(v.getId() == R.id.btTestar)
         {
@@ -51,8 +62,27 @@ public class RegisterOcActivity extends AppCompatActivity
             TextView teste2 = findViewById(R.id.labelTesteContato);
             teste.setText(""+nome);
             teste2.setText(""+contato);
-
         }
     }
 
+    public void takePicture( View view )
+    {
+        Intent imageTakeIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if( imageTakeIntent.resolveActivity( getPackageManager()) != null)
+        {
+            startActivityForResult( imageTakeIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if( requestCode == REQUEST_IMAGE_CAPTURE  &&  resultCode == RESULT_OK )
+        {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap( imageBitmap );
+        }
+    }
 }
