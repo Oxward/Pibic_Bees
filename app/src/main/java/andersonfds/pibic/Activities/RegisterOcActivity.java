f71d53f016d73f5409f60e7d58cb908faf74fa90
Oxward <andersonfreiretads@gmail.com>
2018-09-12 09:41:35 -0300

    Adicionada função de pegar imagens da galeria

--- app/src/main/java/andersonfds/pibic/Activities/RegisterOcActivity.java
+++ app/src/main/java/andersonfds/pibic/Activities/RegisterOcActivity.java
@@ -2,10 +2,12 @@ package andersonfds.pibic.Activities;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -16,6 +18,8 @@ public class RegisterOcActivity extends AppCompatActivity
 {
 
     private ImageView imageView;
+    private Button btGalery;
+
     public static final int REQUEST_IMAGE_CAPTURE = 101;
     public static final int PICK_IMAGE = 1;
 
@@ -24,6 +28,26 @@ public class RegisterOcActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_oc);
         imageView = findViewById( R.id.imgTiraFoto );
+        imageView.setOnClickListener(view -> {
+            takePicture(view);
+        });
+
+        btGalery = findViewById(R.id.btGal);
+        btGalery.setOnClickListener(view -> {
+            openGallery();
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        } else if (requestCode == PICK_IMAGE && resultCode == RESULT_OK) {
+            Uri uri = data.getData();
+            imageView.setImageURI(uri);
+        }
     }
 
     public void buttonPress( View view)
@@ -64,7 +88,7 @@ public class RegisterOcActivity extends AppCompatActivity
         }
     }
 
-    public void takePicture( View view )
+    private void takePicture(View view)
     {
         Intent imageTakeIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
@@ -74,14 +98,10 @@ public class RegisterOcActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        if( requestCode == REQUEST_IMAGE_CAPTURE  &&  resultCode == RESULT_OK )
-        {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imageView.setImageBitmap( imageBitmap );
-        }
+    private void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
     }
+
+
 }
