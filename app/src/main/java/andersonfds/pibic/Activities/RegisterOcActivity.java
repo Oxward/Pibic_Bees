45d0bad3a000259b4f1d6cc342e4d4f128f5ad15
Oxward <andersonfreiretads@gmail.com>
2018-09-12 09:45:57 -0300

    Adicionada função de pegar imagens da galeria

--- app/src/main/java/andersonfds/pibic/Activities/RegisterOcActivity.java
+++ app/src/main/java/andersonfds/pibic/Activities/RegisterOcActivity.java
@@ -40,6 +40,7 @@ public class RegisterOcActivity extends AppCompatActivity
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
