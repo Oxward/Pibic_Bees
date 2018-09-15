4532ede0b9d5a274548884fdac0b1e790974f61d
Oxward <andersonfreiretads@gmail.com>
2018-09-15 10:51:28 -0300

    Sending user current location with the RegisterOcActivity

--- app/src/main/java/andersonfds/pibic/Activities/RegisterOcActivity.java
+++ app/src/main/java/andersonfds/pibic/Activities/RegisterOcActivity.java
@@ -1,20 +1,29 @@
 package andersonfds.pibic.Activities;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
+import android.os.Looper;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 
@@ -22,26 +31,90 @@ import java.nio.file.Files;
 
 import andersonfds.pibic.R;
 
-public class RegisterOcActivity extends AppCompatActivity
-{
+public class RegisterOcActivity extends AppCompatActivity {
 
     //Constantes
-    public static final int REQUEST_IMAGE_CAPTURE = 101;
+    private static final int REQUEST_IMAGE_CAPTURE = 101;
+    private static final int REQUEST_CODE = 1000;
 
     private ImageView imageView;
-    private FusedLocationProviderClient mFusedLocationClient;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private LocationRequest mLocationRequest;
+    private LocationCallback mLocationCallback;
+    private TextView lbTest;
+    private Button btEnv;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_oc);
 
-        imageView = findViewById( R.id.imgTiraFoto );
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        imageView = findViewById(R.id.imgTiraFoto);
+        lbTest = findViewById(R.id.lbTest);
+        btEnv = findViewById(R.id.btEnv);
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+        } else
+          {
+            //Permissions granted
+            buildLocationRequest();
+            buildLocationCallback();
+
+            //fusedProviderClient
+            mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this );
+
+            btEnv.setOnClickListener(view ->
+            {
+              if (ActivityCompat.checkSelfPermission(RegisterOcActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                  PackageManager.PERMISSION_GRANTED &&
+                  ActivityCompat.checkSelfPermission(RegisterOcActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                  PackageManager.PERMISSION_GRANTED)
+                {
+
+                  return;
+                }
+                mFusedLocationProviderClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+            });
+        }
+
     }
 
-    public void buttonPress( View view)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if( requestCode == REQUEST_IMAGE_CAPTURE  &&  resultCode == RESULT_OK )
+        {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap( imageBitmap );
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch( requestCode )
+        {
+            case REQUEST_CODE:
+            {
+                if( grantResults.length > 0)
+                {
+                    if( grantResults[0] == PackageManager.PERMISSION_GRANTED )
+                    {
+
+                    }
+                    else
+                      if( grantResults[0] == PackageManager.PERMISSION_DENIED)
+                      {
+
+                      }
+                }
+            }
+        }
+    }
+
+    public void buttonPress(View view)
     {
         switch ( view.getId() )
         {
@@ -61,24 +134,6 @@ public class RegisterOcActivity extends AppCompatActivity
         }
     }
 
-    public void buttonTeste( View v )
-    {
-        if(v.getId() == R.id.btTestar)
-        {
-            EditText txtNome = findViewById(R.id.txtNome);
-            EditText txtCont = findViewById(R.id.txtCont);
-
-            String nome, contato;
-            nome = txtNome.getText().toString();
-            contato = txtCont.getText().toString();
-
-            TextView teste = findViewById(R.id.labelTesteNome);
-            TextView teste2 = findViewById(R.id.labelTesteContato);
-            teste.setText(""+nome);
-            teste2.setText(""+contato);
-        }
-    }
-
     public void takePicture( View view )
     {
         Intent imageTakeIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -89,22 +144,28 @@ public class RegisterOcActivity extends AppCompatActivity
         }
     }
 
-    protected void createLocationRequest()
+    private void buildLocationRequest()
     {
-        LocationRequest mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(10000);
-        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest = new LocationRequest();
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(5000);
+        mLocationRequest.setFastestInterval(3000);
+        mLocationRequest.setSmallestDisplacement(10);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    private void buildLocationCallback()
     {
-        if( requestCode == REQUEST_IMAGE_CAPTURE  &&  resultCode == RESULT_OK )
+        mLocationCallback = new LocationCallback()
         {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imageView.setImageBitmap( imageBitmap );
-        }
+            @Override
+            public void onLocationResult(LocationResult locationResult)
+            {
+                for ( Location l : locationResult.getLocations() )
+                {
+                    lbTest.setText( String.valueOf( l.getLatitude() +"//"+String.valueOf(l.getLongitude()) ));
+                }
+            }
+        };
     }
+
 }
