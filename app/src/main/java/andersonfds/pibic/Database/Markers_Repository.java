4389a7c9010215475c67de67ceca9c67953fbc32
Oxward <andersonfreiretads@gmail.com>
2018-09-24 17:36:52 -0300

    Loading Map markers with Thread #Fail

--- app/src/main/java/andersonfds/pibic/Database/Markers_Repository.java
+++ app/src/main/java/andersonfds/pibic/Database/Markers_Repository.java
@@ -2,7 +2,6 @@ package andersonfds.pibic.Database;
 
 import android.app.Application;
 import android.os.AsyncTask;
-import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,7 +25,6 @@ public class Markers_Repository {
 
     public List<Markers> getAllMarkers() {
         allMarkers.addAll(markerDAO.selectAllMarkers());
-        //new Thread( new Task() ).start();
         return allMarkers;
     }
 
@@ -34,20 +32,6 @@ public class Markers_Repository {
         new insertAsyncTask(markerDAO).execute(markers);
     }
 
-    private class Task implements Runnable {
-
-        private static final String TAG = "Task";
-
-        @Override
-        public void run() {
-            allMarkers.addAll(markerDAO.selectAllMarkers());
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                Log.d(TAG, "run: Deu pau." + e.getMessage());
-            }
-        }
-    }
 
     private static class insertAsyncTask extends AsyncTask<Markers, Void, Void> {
         private Markers_DAO mAsyncTaskMarkerDAO;
