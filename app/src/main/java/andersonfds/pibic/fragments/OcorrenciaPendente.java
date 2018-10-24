46a6a8b15bcd30c05320f0aafbbd5b4945a585ee
Oxward <andersonfreiretads@gmail.com>
2018-10-24 12:22:07 -0200

    Report

---
+++ app/src/main/java/andersonfds/pibic/fragments/OcorrenciaPendente.java
@@ -0,0 +1,50 @@
+package andersonfds.pibic.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import andersonfds.pibic.R;
+
+public class OcorrenciaPendente extends Fragment {
+
+    private static final String TAG = "OcorrenciaPendente";
+
+    /**
+     * Called to have the fragment instantiate its user interface view.
+     * This is optional, and non-graphical fragments can return null (which
+     * is the default implementation).  This will be called between
+     * {@link #onCreate(Bundle)} and {@link #onActivityCreated(Bundle)}.
+     * <p>
+     * <p>If you return a View from here, you will later be called in
+     * {@link #onDestroyView} when the view is being released.
+     *
+     * @param inflater           The LayoutInflater object that can be used to inflate
+     *                           any views in the fragment,
+     * @param container          If non-null, this is the parent view that the fragment's
+     *                           UI should be attached to.  The fragment should not add the view itself,
+     *                           but this can be used to generate the LayoutParams of the view.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     *                           from a previous saved state as given here.
+     * @return Return the View for the fragment's UI, or null.
+     */
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_ocorrencia_pendente, container, false);
+
+        FloatingActionButton floatingActionButton = view.findViewById(R.id.fabPen);
+
+        floatingActionButton.setOnClickListener(v -> {
+            Snackbar.make(view, "Deu certo " + TAG, Snackbar.LENGTH_SHORT);
+        });
+
+        return view;
+    }
+}
