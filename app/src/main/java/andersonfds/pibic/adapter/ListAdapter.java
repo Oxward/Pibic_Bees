458432c74d0f5eb79cb4943e76d02e3fcfd9a79e
Oxward <andersonfreiretads@gmail.com>
2018-10-29 12:54:51 -0200

    asdf

--- app/src/main/java/andersonfds/pibic/adapter/ListAdapter.java
+++ app/src/main/java/andersonfds/pibic/adapter/ListAdapter.java
@@ -6,10 +6,22 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.nostra13.universalimageloader.cache.memory.impl.WeakMemoryCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
 
 import java.util.ArrayList;
 
+import andersonfds.pibic.R;
 import andersonfds.pibic.classes.RegisterContacts;
 
 public class ListAdapter extends ArrayAdapter<RegisterContacts> {
@@ -18,16 +30,15 @@ public class ListAdapter extends ArrayAdapter<RegisterContacts> {
 
     private Context mContext;
     private int mResource;
-
-    public ListAdapter(Context mContext, int resource, ArrayList<RegisterContacts> mList) {
-        super(mContext, resource, mList);
-        this.mContext = mContext;
-        mResource = resource;
-    }
+    private int lastPosition = -1;
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        //sets up the image loader
+        setupImageLoader();
+
         String name = getItem(position).getNome();
         String cont = String.valueOf(getItem(position).getNumTel());
         String wpp = String.valueOf(getItem(position).getNumWpp());
@@ -36,9 +47,78 @@ public class ListAdapter extends ArrayAdapter<RegisterContacts> {
 
         RegisterContacts pessoa = new RegisterContacts(name, cont, wpp, img1, img2);
 
-        LayoutInflater mLayoutInflater = LayoutInflater.from(mContext);
-        convertView = mLayoutInflater.inflate(mResource, parent, false);
+        final View result;
+        ViewHolder mViewHolder;
+
+        if (convertView == null) {
+            LayoutInflater mLayoutInflater = LayoutInflater.from(mContext);
+            convertView = mLayoutInflater.inflate(mResource, parent, false);
+
+            mViewHolder = new ViewHolder();
+            mViewHolder.name = convertView.findViewById(R.id.TVnome);
+            mViewHolder.cont = convertView.findViewById(R.id.TVcont);
+            mViewHolder.wpp = convertView.findViewById(R.id.TVwpp);
+            mViewHolder.img1 = convertView.findViewById(R.id.IVimg1);
+
+            result = convertView;
+            convertView.setTag(mViewHolder);
+        } else {
+            mViewHolder = (ViewHolder) convertView.getTag();
+            result = convertView;
+        }
+
+
+        Animation mAnimation = AnimationUtils.loadAnimation(mContext,
+                (position > lastPosition) ? R.anim.load_down_anim : R.anim.load_up_anim);
+        result.startAnimation(mAnimation);
+        lastPosition = position;
+
+        int defaultImage = mContext.getResources().getIdentifier("@drawable/image_failed", null, mContext.getPackageName());
+
+        ImageLoader imageLoader = ImageLoader.getInstance();
+        DisplayImageOptions options = new DisplayImageOptions.Builder().cacheInMemory(true)
+                .cacheOnDisk(true).resetViewBeforeLoading(true)
+                .showImageForEmptyUri(defaultImage)
+                .showImageOnFail(defaultImage)
+                .showImageOnLoading(defaultImage).build();
+
+        imageLoader.displayImage("", mViewHolder.img1, options);
+
+        mViewHolder.name.setText(pessoa.getNome());
+        mViewHolder.cont.setText(pessoa.getNumTel());
+        mViewHolder.wpp.setText(pessoa.getNumWpp());
 
         return convertView;
+    }
+
+    public ListAdapter(Context mContext, int resource, ArrayList<RegisterContacts> mList) {
+        super(mContext, resource, mList);
+        this.mContext = mContext;
+        mResource = resource;
+    }
+
+    private void setupImageLoader() {
+        // UNIVERSAL IMAGE LOADER SETUP
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+                .cacheOnDisk(true).cacheInMemory(true)
+                .imageScaleType(ImageScaleType.EXACTLY)
+                .displayer(new FadeInBitmapDisplayer(300)).build();
+
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(
+                getContext())
+                .defaultDisplayImageOptions(defaultOptions)
+                .memoryCache(new WeakMemoryCache())
+                .diskCacheSize(100 * 1024 * 1024).build();
+
+        ImageLoader.getInstance().init(config);
+        // END - UNIVERSAL IMAGE LOADER SETUP
+    }
+
+    static class ViewHolder {
+        TextView name;
+        TextView cont;
+        TextView wpp;
+        ImageView img1;
+        ImageView img2;
     }
 }
