1653abbb5905343ac5961e7f68b7937ad0ac526f
Oxward <andersonfreiretads@gmail.com>
2019-03-27 15:36:39 -0300

    as

---
+++ app/src/main/java/andersonfds/pibic/classes/Products.java
@@ -0,0 +1,84 @@
+package andersonfds.pibic.classes;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "products")
+public class Products {
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "idProd", typeAffinity = ColumnInfo.INTEGER)
+    private int idProd;
+
+    @NonNull
+    @ColumnInfo(name = "descProd", typeAffinity = ColumnInfo.TEXT)
+    private String descProd;
+
+    @NonNull
+    @ColumnInfo(name = "valorProd", typeAffinity = ColumnInfo.REAL)
+    private double valorProd;
+
+    @NonNull
+    @ColumnInfo(name = "imgProd", typeAffinity = ColumnInfo.BLOB)
+    private byte[] imgProd;
+
+    @Ignore
+    public Products() {
+    }
+
+    @Ignore
+    public Products(String descProd, double valorProd) {
+        this.descProd = descProd;
+        this.valorProd = valorProd;
+    }
+
+    @Ignore
+    public Products(String descProd, double valorProd, byte[] imgProd) {
+        this.descProd = descProd;
+        this.valorProd = valorProd;
+        this.imgProd = imgProd;
+    }
+
+    @Ignore
+    public Products(int idProd, String descProd, double valorProd, byte[] imgProd) {
+        this.idProd = idProd;
+        this.descProd = descProd;
+        this.valorProd = valorProd;
+        this.imgProd = imgProd;
+    }
+
+    public int getIdProd() {
+        return idProd;
+    }
+
+    public void setIdProd(int idProd) {
+        this.idProd = idProd;
+    }
+
+    public String getDescProd() {
+        return descProd;
+    }
+
+    public void setDescProd(String descProd) {
+        this.descProd = descProd;
+    }
+
+    public double getValorProd() {
+        return valorProd;
+    }
+
+    public void setValorProd(double valorProd) {
+        this.valorProd = valorProd;
+    }
+
+    public byte[] getImgProd() {
+        return imgProd;
+    }
+
+    public void setImgProd(byte[] imgProd) {
+        this.imgProd = imgProd;
+    }
+}
