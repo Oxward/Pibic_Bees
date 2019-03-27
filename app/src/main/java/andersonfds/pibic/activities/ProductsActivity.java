0ba589d2cdaa1a2143c04502d2388e27448ba0bf
Oxward <andersonfreiretads@gmail.com>
2019-03-27 15:19:32 -0300

    as

---
+++ app/src/main/java/andersonfds/pibic/activities/ProductsActivity.java
@@ -0,0 +1,18 @@
+package andersonfds.pibic.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import andersonfds.pibic.R;
+
+public class ProductsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_products);
+    }
+
+
+
+}
